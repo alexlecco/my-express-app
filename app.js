d6823cf9476098e3598d92d17f320080f609c1a1
alexlecco <alexlecco24@gmail.com>
2020-05-11 13:18:37 -0300

    create express server with mongo db

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-import express from 'express';
+var express        = require("express"),
+    app            = express(),
+    http           = require("http"),
+    server         = http.createServer(app),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    mongoose       = require('mongoose');
 
-const app = express();
-const port = 3000;
+var TVShowCtrl = require('./controllers/tvshows');
 
-app.get('/', (req, res) => {
-    res.send("hello world");
+// API routes
+var tvshows = express.Router();
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+//----
+
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+	if(err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	} else {
+		console.log('Connected to Database');
+	}
+});
+require('./models/tvshow');
+
+app.use('/api', tvshows);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log("Listen on port " + port));
+router.get('/tvshows', function(req, res) {
+  console.log("tvshows::::::::", tvshows)
+  res.json(tvshows)
+});
+
+app.use(router);
+
+server.listen(3001, function() {
+  console.log("Node server running on http://localhost:3001");
+});
